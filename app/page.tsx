7bba6e2a76cb63eebe32e484e80eca16ea554f59
Henintsoa <henintsoahasimanitriniainajoni@gmail.com>
2025-10-10 22:45:09 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,100 @@
-import Image from "next/image";
+"use client";
+
+import { signIn, useSession, signOut } from "next-auth/react";
+import { SiAegisauthenticator } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    const { data: session } = useSession();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    const handleSignin = () => {
+        signIn("github");
+    };
+
+    const handleLogout = () => {
+        const confirmation = window.confirm("Are you sure to logout ?");
+        if (confirmation) {
+            signOut();
+        }
+    };
+
+    if (session) {
+        console.log(session);
+    }
+    return (
+        <>
+            <div className="w-full h-screen">
+                <header className="w-full flex flex-col">
+                    <nav className="w-full flex flex-row justify-between items-center py-2 border-b-3 border-black/10">
+                        <h3 className="flex items-center gap-2 text-xl font-bold text-black/80">
+                            <SiAegisauthenticator />
+                            <span>NextAuth</span>
+                        </h3>
+                        {!session ? (
+                            <button
+                                onClick={handleSignin}
+                                className="flex items-center gap-2 bg-black/50 text-center text-sm text-white font-bole px-4 py-2 rounded-md hover:bg-black/60 cursor-pointer"
+                            >
+                                <FaGithub size={20} />
+                                <span>Singin with github</span>
+                            </button>
+                        ) : (
+                            <FaGithub size={30} />
+                        )}
+                    </nav>
+                </header>
+                <main>
+                    {!session ? (
+                        <div className="w-full h-screen my-8 mx-auto text-center">
+                            <p className="text-3xl text-red-300">
+                                You are not connected !
+                            </p>
+                            <p className="text-black/50 font-bold text-lg">
+                                You can try to connect with you github account
+                            </p>
+                        </div>
+                    ) : (
+                        <div className="w-full flex flex-col gap-4 my-4">
+                            <p className="text-xl lg:text-3xl font-bold text-green-500">
+                                You are now connected
+                            </p>
+                            <div className="w-full flex flex-col md:flex-row gap-4 md:items-center">
+                                <img
+                                    src={session.user?.image as string}
+                                    alt="profile github"
+                                    className="w-[80px] rounded-full"
+                                />
+                                <div className="w-full flex flex-col gap-1">
+                                    <p className="text-sm text-black/50">
+                                        <span className="font-bold underline">
+                                            Username:{" "}
+                                        </span>
+                                        {session.user?.name}
+                                    </p>
+                                    <p className="text-sm text-black/50">
+                                        <span className="font-bold underline">
+                                            Email:{" "}
+                                        </span>
+                                        {session.user?.email}
+                                    </p>
+                                </div>
+                            </div>
+                            <p className="opacity-80">
+                                token expire:{" "}
+                                <span className="font-bold">
+                                    {session.expires}
+                                </span>
+                            </p>
+                            <button
+                                onClick={handleLogout}
+                                className="px-4 py-2 bg-red-400 text-white font-bold text-sm rounded-md cursor-pointer hover:bg-red-500"
+                            >
+                                Log out
+                            </button>
+                        </div>
+                    )}
+                </main>
+            </div>
+        </>
+    );
 }
