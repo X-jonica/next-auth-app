231d470b29edb9c284266a029f6aa5a39a3a598b
Henintsoa <henintsoahasimanitriniainajoni@gmail.com>
2025-10-18 21:34:47 +0300

    feat(google): add google authentification
    - to allows user to sign in with google
    - create new project on google cloud
    - create client at "clients OAuth 2.0"

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@
 import { signIn, useSession, signOut } from "next-auth/react";
 import { SiAegisauthenticator } from "react-icons/si";
 import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { LuArrowRightLeft } from "react-icons/lu";
+import { GoDotFill } from "react-icons/go";
 
 export default function Home() {
     const { data: session } = useSession();
 
-    const handleSignin = () => {
-        signIn("github");
-    };
+    const handleSigninGithub = () => signIn("github");
+
+    const handleSigninGoogle = () => signIn("google");
 
     const handleLogout = () => {
         const confirmation = window.confirm("Are you sure to logout ?");
@@ -21,6 +24,7 @@ export default function Home() {
     if (session) {
         console.log(session);
     }
+
     return (
         <>
             <div className="w-full h-screen">
@@ -31,15 +35,32 @@ export default function Home() {
                             <span>NextAuth</span>
                         </h3>
                         {!session ? (
-                            <button
-                                onClick={handleSignin}
-                                className="flex items-center gap-2 bg-black/50 text-center text-sm text-white font-bole px-4 py-2 rounded-md hover:bg-black/60 cursor-pointer"
-                            >
-                                <FaGithub size={20} />
-                                <span>Singin with github</span>
-                            </button>
+                            <div className="flex gap-2">
+                                <button
+                                    onClick={handleSigninGithub}
+                                    className="flex items-center gap-2 bg-black/50 text-center text-sm text-white font-bole px-4 py-2 rounded-md hover:bg-black/60 cursor-pointer"
+                                >
+                                    <FaGithub size={20} />
+                                    <span className="hidden md:block">
+                                        Sign in with github
+                                    </span>
+                                </button>
+                                <button
+                                    onClick={handleSigninGoogle}
+                                    className="flex items-center gap-2 border-3 border-blue-500/50 text-center text-sm text-black/80 font-bole px-4 py-2 rounded-md hover:border-blue-500/60 cursor-pointer"
+                                >
+                                    <FcGoogle size={20} />
+                                    <span className="hidden md:block">
+                                        Sign in with google
+                                    </span>
+                                </button>
+                            </div>
                         ) : (
-                            <FaGithub size={30} />
+                            <div className="flex items-center gap-4 px-2">
+                                <FaGithub size={30} />
+                                <LuArrowRightLeft size={20} />
+                                <FcGoogle size={30} />
+                            </div>
                         )}
                     </nav>
                 </header>
@@ -50,37 +71,42 @@ export default function Home() {
                                 You are not connected !
                             </p>
                             <p className="text-black/50 font-bold text-lg">
-                                You can try to connect with you github account
+                                You can try to connect with you github or google
+                                account
                             </p>
                         </div>
                     ) : (
-                        <div className="w-full flex flex-col gap-4 my-4">
+                        <div className="w-full flex flex-col gap-6 my-4">
                             <p className="text-xl lg:text-3xl font-bold text-green-500">
                                 You are now connected
                             </p>
-                            <div className="w-full flex flex-col md:flex-row gap-4 md:items-center">
+                            <div className="w-full flex flex-col items-center md:flex-row gap-4 md:items-center">
                                 <img
                                     src={session.user?.image as string}
                                     alt="profile github"
-                                    className="w-[80px] rounded-full"
+                                    className="w-[90px] rounded-full mx-auto"
                                 />
-                                <div className="w-full flex flex-col gap-1">
-                                    <p className="text-sm text-black/50">
-                                        <span className="font-bold underline">
+                                <div className="w-full overflow-x-scroll flex flex-col gap-1">
+                                    <p className="flex gap-2 items-center text-black/50">
+                                        <span className="underline w-20 sm:w-25 sm:text-lg">
                                             Username:{" "}
                                         </span>
-                                        {session.user?.name}
+                                        <span className="font-bold text-lg">
+                                            {session.user?.name}
+                                        </span>
                                     </p>
-                                    <p className="text-sm text-black/50">
-                                        <span className="font-bold underline">
+                                    <p className="flex gap-2 items-center text-black/50">
+                                        <span className="underline w-20 sm:w-25 sm:text-lg">
                                             Email:{" "}
                                         </span>
-                                        {session.user?.email}
+                                        <span className="font-bold text-lg">
+                                            {session.user?.email}
+                                        </span>
                                     </p>
                                 </div>
                             </div>
                             <p className="opacity-80">
-                                token expire:{" "}
+                                Your token:{" "}
                                 <span className="font-bold">
                                     {session.expires}
                                 </span>
